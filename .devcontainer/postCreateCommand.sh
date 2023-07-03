de71823f0d1e7f576bb2637653db2aadb9ac8e12
Martin Jochum <mjochum@juris.de>
2023-07-03 17:20:19 +0200

    tasks eingebaut für login

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,15 +7,15 @@ unzip CascadiaCode.zip -d $HOME/.local/share/fonts
 rm CascadiaCode.zip
 
 # ## AZURE CLI EXTENSIONS ##
-# extensions=$(az extension list-available --query "[].name" | jq -c -r '.[]')
-# for extension in $extensions;
-# do
-#     az extension add --name $extension
-# done
+extensions=$(az extension list-available --query "[].name" | jq -c -r '.[]')
+for extension in $extensions;
+do
+    az extension add --name $extension
+done
 
 ## OH-MY-ZSH PLUGINS & THEMES ##
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-# git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-# echo "$(cat $HOME/.zshrc)" | awk '{gsub(/ZSH_THEME=\"codespaces\"/, "ZSH_THEME=\"agnoster\"")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
-#echo "$(cat $HOME/.zshrc)" | awk '{gsub(/plugins=\(git\)/, "plugins=(git terraform zsh-syntax-highlighting zsh-autosuggestions)")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
+echo "$(cat $HOME/.zshrc)" | awk '{gsub(/ZSH_THEME=\"codespaces\"/, "ZSH_THEME=\"agnoster\"")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
+echo "$(cat $HOME/.zshrc)" | awk '{gsub(/plugins=\(git\)/, "plugins=(git terraform zsh-syntax-highlighting zsh-autosuggestions)")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
--- .devcontainer/preContainerStart.sh
+++ .devcontainer/preContainerStart.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
-echo "Starting Pre-Container-Start-Jobs ..."
+# Setzen von git-config-Werten, von denen bekannt ist, dass sie Git-Fehler beheben
+# Line endings (#4069)
+git config core.eol lf
+git config core.autocrlf false
+# zeroPaddedFilemode fsck errors (#4963)
+git config fsck.zeroPaddedFilemode ignore
+git config fetch.fsck.zeroPaddedFilemode ignore
+git config receive.fsck.zeroPaddedFilemode ignore
+# autostash on rebase (#7172)
+resetAutoStash=$(git config --bool rebase.autoStash 2>/dev/null)
+git config rebase.autoStash true
+
+RED=$(printf '\033[31m')
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+BLUE=$(printf '\033[34m')
+BOLD=$(printf '\033[1m')
+DIM=$(printf '\033[2m')
+UNDER=$(printf '\033[4m')
+RESET=$(printf '\033[m')
+
+printf '\033[38;5;196m'
+printf '                _       __              __     \n' $RESET
+printf '\033[38;5;202m'
+printf '   ____ ___    (_)     / / _____ ____  / /____ \n' $RESET
+printf '\033[38;5;226m'
+printf '  / __ `__ \  / /_____/ __/ __ \/ __ \/ / ___/ \n' $RESET
+printf '\033[38;5;082m'
+printf ' / / / / / / / /_____/ /_/ /_/ / /_/ / (__  )  \n' $RESET
+printf '\033[38;5;021m'
+printf '/_/ /_/ /_/_/ /      \__/\____/\____/_/____/   \n' $RESET
+printf '\033[38;5;093m'
+printf '         /___/                                 \n' $RESET
+printf '\n'
+printf "${BLUE}%s${RESET}\n" "Starting Pre-Container-Start-Jobs"
+printf '\n' $RESET
 
 # if docker search ${REGISTRY_NAME}/ --limit 1 &> /dev/null;
 # then
